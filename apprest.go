01cbdb0b5cd513e5bb81429a975427af249638b6
Doug Davis <dug@us.ibm.com>
2021-04-08 00:54:15 +0000

    more
    Signed-off-by: Doug Davis <dug@us.ibm.com>

--- apprest.go
+++ apprest.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"strings"
@@ -83,10 +84,111 @@ func CreateApp(name string, image string) error {
 	return nil
 }
 
+func DeleteApp(name string) error {
+	path := "/apis/serving.knative.dev/v1/namespaces/" + Namespace +
+		"/services/" + name
+	code, body, err := KubeCall("DELETE", path, "")
+	if code/100 != 2 {
+		if err != nil {
+			return err
+		}
+		return fmt.Errorf("%d: %s", code, body)
+	}
+	return nil
+}
+
+func GetAppStatus(name string) (bool, error) {
+	path := "/apis/serving.knative.dev/v1/namespaces/" + Namespace +
+		"/services/" + name
+	code, body, err := KubeCall("GET", path, "")
+	if code/100 != 2 {
+		if err != nil {
+			return false, err
+		}
+		return false, fmt.Errorf("%d: %s", code, body)
+	}
+
+	/* APP's STATUS  JSON:
+	   "status": {
+	       "address": {
+	           "url": "http://echo2.79gf3v2htsc.svc.cluster.local"
+	       },
+	       "conditions": [
+	           {
+	               "lastTransitionTime": "2021-04-08T00:52:19Z",
+	               "status": "True",
+	               "type": "ConfigurationsReady"
+	           },
+	           {
+	               "lastTransitionTime": "2021-04-08T00:52:26Z",
+	               "status": "True",
+	               "type": "Ready"
+	           },
+	           {
+	               "lastTransitionTime": "2021-04-08T00:52:26Z",
+	               "status": "True",
+	               "type": "RoutesReady"
+	           }
+	       ],
+	       "latestCreatedRevisionName": "echo2-00001",
+	       "latestReadyRevisionName": "echo2-00001",
+	       "observedGeneration": 1,
+	       "traffic": [
+	           {
+	               "latestRevision": true,
+	               "percent": 100,
+	               "revisionName": "echo2-00001"
+	           }
+	       ],
+	       "url": "http://echo2.79gf3v2htsc.us-south.codeengine.appdomain.cloud"
+	   }
+	*/
+
+	raw := json.RawMessage{}
+	res := map[string]json.RawMessage{}
+	json.Unmarshal([]byte(body), &res)
+	if raw = (res["status"]); raw == nil {
+		return false, nil
+	}
+	json.Unmarshal(raw, &res)
+	conditions := []struct {
+		LastTransitionTime string
+		Status             string
+		Type               string
+	}{}
+	raw = res["conditions"]
+	json.Unmarshal(raw, &conditions)
+	for _, c := range conditions {
+		if c.Type == "Ready" {
+			return c.Status == "True", nil
+		}
+	}
+	return false, nil
+}
+
+func WaitForApp(name string) error {
+	for {
+		ready, err := GetAppStatus(name)
+		if err != nil {
+			return err
+		}
+		if ready {
+			return nil
+		}
+		time.Sleep(time.Second)
+	}
+}
+
 func main() {
 	err := CreateApp("echo2", "duglin/echo")
 	if err != nil {
 		fmt.Printf("%s\n", err)
 		os.Exit(1)
 	}
+	err = WaitForApp("echo2")
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("App is ready\n")
 }
